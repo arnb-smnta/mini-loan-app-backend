03f061c082ce3c6a30dfa62f42c207d09f763324
Arnab Samanta <loveumearnab.2812000@gmail.com>
2024-11-21 17:38:34 +0530

    .

--- src/app.js
+++ src/app.js
@@ -81,12 +81,6 @@ import loanRouter from "./routes/apps/mini-loan-app/mini-loan-app.routes.js";
 // * App apis
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/loans/", loanRouter);
-
-app.get("/", (req, res) => {
-  res.redirect(
-    `https://github.com/arnb-smnta/expense-split-prod-backend/blob/main/src/app.js`
-  );
-});
 app.get("/", (req, res) => {
   res.redirect(`${process.env.DOCUMENTATION_URL}`);
 });
